32ff9bdad4e312a501736eb76a6472b1d0faea9b
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-08 22:49:58 +0530

    add event listener to next button

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -4,3 +4,11 @@ const next = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
 
 let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+});

c3662cf27c87d1112003afe859200a305e22605c
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-08 23:04:33 +0530

    add update function

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -11,6 +11,8 @@ next.addEventListener("click", () => {
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
+
+  update();
 });
 
 prev.addEventListener("click", () => {
@@ -19,4 +21,16 @@ prev.addEventListener("click", () => {
   if (currentActive < 1) {
     currentActive = 1;
   }
+
+  update();
 });
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+}

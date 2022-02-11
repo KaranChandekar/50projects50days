674cb095c1a17fcb90f57e90fa10a1f61a6110cd
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-11 20:22:48 +0530

    add set and clear intervals

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -3,6 +3,12 @@ const bg = document.querySelector(".bg");
 
 let load = 0;
 
+let int = setInterval(blurring, 30);
+
 function blurring() {
   load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
 }

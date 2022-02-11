7e51cd6835819fdc0b4e8f6d6679cb450a9745bc
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-11 20:35:29 +0530

    style background filter

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -14,6 +14,7 @@ function blurring() {
 
   loadText.innerText = `${load}%`;
   loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {

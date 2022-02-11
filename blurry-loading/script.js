2b4d9f5a0fc6b0bc9f16aa6d779255cb80f217cc
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-11 20:29:07 +0530

    add scale function

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -14,3 +14,7 @@ function blurring() {
 
   loadText.innerText = `${load}%`;
 }
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};

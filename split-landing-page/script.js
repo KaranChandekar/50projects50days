b1eb0a14892c00ed74956e1eb876cf1daa1648ce
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-13 21:46:29 +0530

    add event listener to left

--- split-landing-page/script.js
+++ split-landing-page/script.js
@@ -1,3 +1,10 @@
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const container = document.querySelector(".container");
+
+left.addEventListener("mouseenter", () =>
+  container.classList.add("hover-left")
+);
+left.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-left")
+);

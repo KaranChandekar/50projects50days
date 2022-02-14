4c1a22ee304dba9253eb7e713409f21b95b692d3
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-14 22:06:37 +0530

    add functionality

--- form-input-wave/script.js
+++ form-input-wave/script.js
@@ -1,1 +1,8 @@
 const labels = document.querySelectorAll(".fom-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map((letter, idx) => `<span>${letter}</span>`)
+    .join("");
+});

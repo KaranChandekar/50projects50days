f86860620a34dff78f3a40ef20afca8d9c023c6f
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-14 22:28:10 +0530

    add functionality

--- form-input-wave/script.js
+++ form-input-wave/script.js
@@ -1,8 +1,11 @@
-const labels = document.querySelectorAll(".fom-control label");
+const labels = document.querySelectorAll(".form-control label");
 
 labels.forEach((label) => {
   label.innerHTML = label.innerText
     .split("")
-    .map((letter, idx) => `<span>${letter}</span>`)
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
     .join("");
 });

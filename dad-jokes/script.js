4146e15b818ddcc100e4c14eb33e445ad448c6d9
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-16 21:40:10 +0530

    add generateJoke function

--- dad-jokes/script.js
+++ dad-jokes/script.js
@@ -1,2 +1,13 @@
 const jokeEl = document.getElementById("joke");
 const jokeBtn = document.getElementById("jokeBtn");
+
+generateJoke();
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  fetch("https://icanhazdadjoke.com", config);
+}

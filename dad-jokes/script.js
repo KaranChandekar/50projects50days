616ed7c92abc32fd7d2d278285bb51a83c0aaf8b
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-16 21:55:43 +0530

    add click event on button

--- dad-jokes/script.js
+++ dad-jokes/script.js
@@ -1,6 +1,8 @@
 const jokeEl = document.getElementById("joke");
 const jokeBtn = document.getElementById("jokeBtn");
 
+jokeBtn.addEventListener("click", generateJoke);
+
 generateJoke();
 
 function generateJoke() {
@@ -12,6 +14,6 @@ function generateJoke() {
   fetch("https://icanhazdadjoke.com", config)
     .then((res) => res.json())
     .then((data) => {
-        jokeEl.innerHTML = data.joke
+      jokeEl.innerHTML = data.joke;
     });
 }

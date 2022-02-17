ab130e7510606cbb032adc4e715619cba7c856b1
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-17 22:35:44 +0530

    add html with js

--- event-keycodes/script.js
+++ event-keycodes/script.js
@@ -1,3 +1,18 @@
+const insert = document.getElementById("insert");
+
 window.addEventListener("keydown", (e) => {
-  console.log(e);
+  insert.innerHTML = `
+        <div class="key">
+            a
+            <small>event.key</small>
+        </div>
+        <div class="key">
+            65
+            <small>event.keyCode</small>
+        </div>
+        <div class="key">
+            KeyA
+            <small>event.code</small>
+        </div>
+  `;
 });

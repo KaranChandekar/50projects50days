30bf728680095b98e2c0d734576cba50f252bfe6
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-21 19:37:25 +0530

    add forEach counter

--- incrementing-counter/script.js
+++ incrementing-counter/script.js
@@ -1,1 +1,12 @@
 const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
+
+    const increment = target / 200;
+  };
+});

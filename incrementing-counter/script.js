e19938ccd03b4986f8a7dd88f66343435a6d98a7
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-21 21:22:20 +0530

    add conditions

--- incrementing-counter/script.js
+++ incrementing-counter/script.js
@@ -8,5 +8,14 @@ counters.forEach((counter) => {
     const c = +counter.innerText;
 
     const increment = target / 200;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
   };
+
+  updateCounter();
 });

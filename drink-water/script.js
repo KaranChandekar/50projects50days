83d120da76e22a15c8602d57ab7db8e4541f01ee
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-22 22:57:00 +0530

    add forEach on smallCups

--- drink-water/script.js
+++ drink-water/script.js
@@ -2,3 +2,7 @@ const smallCups = document.querySelectorAll(".cup-small");
 const listers = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remainder = document.getElementById("remainder");
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlightCups(idx));
+});

5c244f9bf6ad4f99f4832c1596b869e4a6c6d5d2
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-22 22:59:56 +0530

    add highlightCups function

--- drink-water/script.js
+++ drink-water/script.js
@@ -6,3 +6,13 @@ const remainder = document.getElementById("remainder");
 smallCups.forEach((cup, idx) => {
   cup.addEventListener("click", () => highlightCups(idx));
 });
+
+function highlightCups(idx) {
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+}

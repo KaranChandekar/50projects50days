fe9615fa35e05754b56bed35b06067e80afda4d9
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-22 22:55:54 +0530

    select elements

---
+++ drink-water/script.js
@@ -0,0 +1,4 @@
+const smallCups = document.querySelectorAll(".cup-small");
+const listers = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remainder = document.getElementById("remainder");

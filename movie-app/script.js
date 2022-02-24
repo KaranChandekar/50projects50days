88f5794d5635aa4914ce7c25894277628ef952bf
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-24 13:31:50 +0530

    add getClassByRate function

--- movie-app/script.js
+++ movie-app/script.js
@@ -41,3 +41,13 @@ function showMovies(movies) {
     main.appendChild(movieEl);
   });
 }
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}

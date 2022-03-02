b9de4a611a1bfa50ae2c9221fea5ba49f1b71e28
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-02 23:16:18 +0530

    grabbing elements with js

---
+++ content-placeholder/script.js
@@ -0,0 +1,9 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
